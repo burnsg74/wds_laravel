91a9c435c44629a0a6f8b8ad72ce0982f75f6edf
Gregory D. Burns <greg@burnswebservices.com>
2019-05-29 13:32:04 -0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-9"
+                 style="background: yellow">
+                <div class="row">
+                    <div class="col-md-12"
+                         style="background: yellow">Income
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3"
+                 style="background: lightskyblue">
+                <div class="row">
+                    <div class="col-md-4">Date</div>
+                    <div class="col-md-4">Item</div>
+                    <div class="col-md-4">Amount</div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
